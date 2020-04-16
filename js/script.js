5672d5f59a1248ed81ea337c385ced3ca260854e
Giovanni Kaaijk <giovanni@goedgemerkt.nl>
2020-04-16 12:21:34 +0200

    styling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const video = document.getElementById('video')
 
 let audio, amplitude
+let toggle = true
 
 function preload () {
   audio = loadSound('audio/f1_rewind.mp3')
@@ -9,11 +10,9 @@ function preload () {
 function setup () {
   createCanvas(0, 0)
   amplitude = new p5.Amplitude()
-
   video.addEventListener('play', e => {
     audio.play(0, 1, 1, e.target.currentTime)
   })
-
   video.addEventListener('pause', e => {
     audio.stop()
   })
@@ -22,8 +21,20 @@ function setup () {
 function draw () {
   background(0)
   let amplitudeLevel = amplitude.getLevel()
+  let fadeElement = document.querySelector('.fade')
+  if(fadeElement) {
+    if(toggle) {
+      toggle = false
 
+      setTimeout(() => {
+        fadeElement.classList.add('hide')
+      }, 1000)
+      setTimeout(() => {
+        toggle = true
+      }, 1000)
+    }
+  }
   if (document.querySelector('span.sound')) {
-    document.querySelector('span.sound').style.fontSize = `${Math.pow((amplitudeLevel + 1), 2)}rem`
+    document.body.style.setProperty('--fontSize', `${Math.pow((amplitudeLevel + 1.2), 3)}rem`);
   }
 }
