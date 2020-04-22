2c5cb3c2b623de1ebc6ff896d992a29ba87f9298
Giovanni Kaaijk <giovanni@goedgemerkt.nl>
2020-04-22 13:26:34 +0200

    more subtitles

--- js/script.js
+++ js/script.js
@@ -17,7 +17,9 @@ function setup () {
     audio.stop()
   })
 }
-
+let slidetoggle = true
+let bigslideToggle = true
+let bigslideLeftToggle = true
 function draw () {
   background(0)
   let amplitudeLevel = amplitude.getLevel()
@@ -35,6 +37,52 @@ function draw () {
     }
   }
   if (document.querySelector('span.sound')) {
-    document.body.style.setProperty('--fontSize', `${Math.pow((amplitudeLevel + 1.2), 3)}rem`);
+    document.body.style.setProperty('--fontSize', `${Math.pow((amplitudeLevel + 1.2), 3.5)}rem`);
   }
+  if(document.querySelector('span.tilt-3')) {
+      let spanParent = document.querySelector('span.tilt-3').parentNode.parentNode.parentNode
+      spanParent.style.transform = 'rotate(2deg)'
+      let captions = document.querySelector('.captions')
+      if(slidetoggle === true) {
+          slidetoggle = false
+        captions.classList.add('slide')
+        setTimeout(() => {
+            captions.classList.remove('slide')
+            spanParent.style.transform = 'rotate(0deg)'
+            slidetoggle = true
+        }, 2000);
+      }
+      
+  }
+  if(document.querySelector('span.bigslide.left')) {
+    if(bigslideLeftToggle === true) {
+        let spanParent = document.querySelector('span.bigslide').parentNode.parentNode.parentNode
+        spanParent.style.transform = 'rotate(-2deg)'
+        let captions = document.querySelector('.captions')
+        bigslideLeftToggle = false
+      captions.classList.add('bigslide-left')
+      setTimeout(() => {
+          captions.classList.remove('bigslide-left')
+          spanParent.style.transform = 'rotate(0deg)'
+          bigslideLeftToggle = true
+      }, 1600);
+    }
+    
+}
+  if(document.querySelector('span.bigslide')) {
+    if(bigslideToggle === true) {
+        console.log('test')
+        let spanParent = document.querySelector('span.bigslide').parentNode.parentNode.parentNode
+        spanParent.style.transform = 'rotate(2deg)'
+        let captions = document.querySelector('.captions')
+        bigslideToggle = false
+      captions.classList.add('bigslide')
+      setTimeout(() => {
+          captions.classList.remove('bigslide')
+          spanParent.style.transform = 'rotate(0deg)'
+          bigslideToggle = true
+      }, 2000);
+    }
+    
+}
 }
